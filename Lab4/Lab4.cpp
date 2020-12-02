3e397da017a8ae869b1ff74be35bf671ffffe47b
Markus Jonsson <64263599+MarkusJonsson1996@users.noreply.github.com>
2020-12-02 10:50:54 +0100

    Add project files.

---
+++ Lab4/Lab4.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "lab4lib.h"
+
+using namespace std;
+
+int main() {
+
+	bool run = true;
+	int task;
+	string uIn;
+
+	while (run) {
+		cout << "Enter the task to run (1-6), \"0\" to exit" << endl;
+		cin >> task;
+
+		switch (task) {
+
+
+
+
+		default:
+			break;
+		}
+
+
+	}
+
+	return 0;
+}
